ec0f1df849f9297c5d863a0df33f8de82251f904
henri <henri@henr.in>
2024-05-05 15:35:37 -0300

    refac: abstract impl skeleton generation

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,45 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote};
 use syn::DeriveInput;
 
 #[proc_macro_derive(Getters)]
 pub fn getters(tokens: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(tokens).unwrap();
-    getters::r#impl(&ast)
+    let getters = gen_getters(&ast);
+
+    generate_methods(&ast, getters).into()
 }
 
-mod getters {
-    use super::*;
-    use syn::FieldsNamed;
+fn generate_methods(ast: &DeriveInput, methods: Vec<TokenStream2>) -> TokenStream2 {
+    let (impl_generics, type_generics, where_clause) = &ast.generics.split_for_impl();
+    let struct_name = &ast.ident;
 
-    pub fn r#impl(ast: &DeriveInput) -> TokenStream {
-        let r#struct = match &ast.data {
-            syn::Data::Struct(r#struct) => r#struct,
-            syn::Data::Enum(_) => panic!("getters can only be generated for structs"),
-            syn::Data::Union(_) => panic!("getters can only be generated for structs"),
-        };
-
-        match &r#struct.fields {
-            syn::Fields::Named(fields) => named_fields(ast, fields),
-            syn::Fields::Unnamed(_fields) => todo!(),
-            syn::Fields::Unit => todo!(),
+    quote! {
+        impl #impl_generics #struct_name #type_generics #where_clause {
+            #(#methods)*
         }
     }
+}
 
-    fn named_fields(ast: &DeriveInput, fields: &FieldsNamed) -> TokenStream {
-        let (impl_generics, type_generics, where_clause) = &ast.generics.split_for_impl();
-        let struct_name = &ast.ident;
+fn gen_getters(ast: &DeriveInput) -> Vec<TokenStream2> {
+    let r#struct = match &ast.data {
+        syn::Data::Struct(r#struct) => r#struct,
+        syn::Data::Enum(_) => panic!("getters can only be generated for structs"),
+        syn::Data::Union(_) => panic!("getters can only be generated for structs"),
+    };
 
-        let generated_methods = fields.named.iter().fold(Vec::new(), |mut acc, field| {
-            let field_name = field.ident.clone().unwrap();
-            let method_name = format_ident!("get_{field_name}");
-            let r#type = &field.ty;
+    r#struct.fields.iter().fold(Vec::new(), |mut acc, field| {
+        let field_name = field.ident.clone().unwrap();
+        let method_name = format_ident!("get_{field_name}");
+        let r#type = &field.ty;
 
-            acc.push(quote! {
-                pub fn #method_name(&self) -> &#r#type {
-                    &self.#field_name
-                }
-            });
-
-            acc
+        acc.push(quote! {
+            pub fn #method_name(&self) -> &#r#type {
+                &self.#field_name
+            }
         });
 
-        quote! {
-            impl #impl_generics #struct_name #type_generics #where_clause {
-                #(#generated_methods)*
-            }
-        }
-        .into()
-    }
+        acc
+    })
 }
