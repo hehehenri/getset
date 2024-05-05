dbe04b506b76bbc1a7d4b987ad6de0f0869625ce
henri <henri@henr.in>
2024-05-05 14:22:13 -0300

    feat: add support for generics and change crate name

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::DeriveInput;
 
-#[proc_macro_derive(Getters, attributes(get))]
+#[proc_macro_derive(Getters)]
 pub fn getters(tokens: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(tokens).unwrap();
     getters::r#impl(&ast)
@@ -15,8 +15,8 @@ mod getters {
     pub fn r#impl(ast: &DeriveInput) -> TokenStream {
         let r#struct = match &ast.data {
             syn::Data::Struct(r#struct) => r#struct,
-            syn::Data::Enum(_) => panic!("Getters can only be generated for structs"),
-            syn::Data::Union(_) => panic!("Getters can only be generated for structs"),
+            syn::Data::Enum(_) => panic!("getters can only be generated for structs"),
+            syn::Data::Union(_) => panic!("getters can only be generated for structs"),
         };
 
         match &r#struct.fields {
@@ -27,9 +27,10 @@ mod getters {
     }
 
     fn named_fields(ast: &DeriveInput, fields: &FieldsNamed) -> TokenStream {
+        let (impl_generics, type_generics, where_clause) = &ast.generics.split_for_impl();
         let struct_name = &ast.ident;
 
-        let methods = fields.named.iter().fold(Vec::new(), |mut acc, field| {
+        let generated_methods = fields.named.iter().fold(Vec::new(), |mut acc, field| {
             let field_name = field.ident.clone().unwrap();
             let method_name = format_ident!("get_{field_name}");
             let r#type = &field.ty;
@@ -44,8 +45,8 @@ mod getters {
         });
 
         quote! {
-            impl #struct_name {
-                #(#methods)*
+            impl #impl_generics #struct_name #type_generics #where_clause {
+                #(#generated_methods)*
             }
         }
         .into()
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,57 @@
-use ezaccess::Getters;
+use getset::Getters;
 
-mod pog {
-    pub mod hehe {
-        pub type Wooooooooooooooow = String;
+#[allow(dead_code)]
+mod example {
+    struct Example {
+        pub public_field: String,
+        private_field: String,
+        hidden_field: String,
+    }
+
+    impl Example {
+        fn get_public_field(&self) -> &String {
+            &self.public_field
+        }
+
+        fn set_public_field(&mut self, value: String) {
+            self.public_field = value
+        }
+
+        fn private_field_mut(&mut self) -> &mut String {
+            &mut self.private_field
+        }
+    }
+
+    fn _example() {
+        let mut example = Example {
+            public_field: "this is a public field".to_string(),
+            private_field: "this is a private field".to_string(),
+            hidden_field: "this should be hidden".to_string(),
+        };
+
+        (*example.private_field_mut()) = "hehe".to_string();
     }
 }
 
 #[derive(Getters)]
-struct Banana {
-    #[get]
-    pub public_field: pog::hehe::Wooooooooooooooow,
+struct Example {
+    pub public_field: String,
+    private_field: String,
+}
+
+#[derive(Getters)]
+struct Dunno<T> {
+    some_field: T,
 }
 
 fn main() {
-    let pog = Banana {
-        public_field: "test".to_string(),
+    let example = Example {
+        public_field: "private public".to_string(),
+        private_field: "private value".to_string(),
     };
 
-    pog.get_public_field();
+    let hehe = Dunno { some_field: "pog" };
+    hehe.get_some_field();
+
+    example.get_public_field();
 }
